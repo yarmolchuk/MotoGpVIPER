a6c3a2bc27ea67d857b81e51cea6d65646975c64
Dima Yarmolchuk <yarmolchuk@gmail.com>
2018-02-07 12:51:45 +0200

    Change ApiService

--- SwiftTestProject/Core/Networking/ApiService.swift
+++ SwiftTestProject/Core/Networking/ApiService.swift
@@ -11,13 +11,35 @@ import Foundation
 // MARK: Protocol
 
 protocol ApiService {
-    func request(with toket:TargetType, complection: @escaping(ApiResponce) -> Void )
+    func request(with toket:TargetType, completion: @escaping(ApiResponce) -> Void )
 }
 
 // MARK: Implementation
 
 private final class ApiServiceImpl: ApiService {
-    func request(with toket: TargetType, complection: @escaping (ApiResponce) -> Void) {
+    func request(with toket: TargetType, completion: @escaping (ApiResponce) -> Void) {
+        
+        let url = URL(string: toket.baseUrl + toket.path)
+        let urlSession = URLSession.shared
+        
+        urlSession.dataTask(with: url!) { (data, response, error) in
+            switch (data, response, error) {
+            case (let data, let response as HTTPURLResponse, nil) where response.statusCode == 200:
+                guard let _data = data else { fallthrough }
+                completion(ApiResponce.success(_data))
+
+            default:
+                guard let _error = error else { break }
+                completion(ApiResponce.failure(_error))
+            }
+        }.resume()
     }
 }
 
+// MARK: Factory
+
+class ApiServiceFactory {
+    static func `default`() -> ApiService {
+        return ApiServiceImpl()
+    }
+}
